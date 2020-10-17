dd109e1a59b32fd08ab3c92c4560f4d21c910c98
Apollon <apol657@gmail.com>
2020-10-17 12:56:26 +0300

    Refactored a few things, resolved runtime Processing deprecation warnings.

--- QuadTreeDemo/core/Core.java
+++ QuadTreeDemo/core/Core.java
@@ -11,218 +11,209 @@ import java.util.Random;
 
 public class Core extends PApplet {
 
-	private static Core instance;
-	private static final long serialVersionUID = 1L;
-	private int width = 800, height = 800;
-	private int fps = 120, actualfps;
-	private long fpsDrawingDelay = 1001;
-	private long t;
-	private QuadTree quadTree;
-	private List<Entity> objects;
-	private List<Entity> tempObjects;
-	private int objectsNum = 500;
-	private Random rng;
-	private boolean shouldColor;
-	private boolean drawingUserRect;
-	private double userX, userY, userX2, userY2;
+    private static final int WINDOW_WIDTH = 800;
+    private static final int WINDOW_HEIGHT = 800;
+    private static final int FPS = 120;
+    private double actualFps;
+    private long fpsDrawingDelay = 1001;
+    private long t;
+    private QuadTree quadTree;
+    private List<Entity> objects;
+    private List<Entity> tempObjects;
+    private int objectsNum = 500;
+    private Random rng;
+    private boolean shouldColor;
+    private boolean drawingUserRect;
+    private double userX;
+    private double userY;
 
-	public void setup() {
-		instance = this;
-		t = millis();
-		frame.setTitle("QuadTreeDemo");
-		rng = new Random();
-		quadTree = new QuadTree(0, new Rectangle(0, 0, width, height), 5, 10);
-		objects = new ArrayList<Entity>();
-		tempObjects = new ArrayList<Entity>();
-		for (int i = 0; i < objectsNum; i++) {
-			int w = rng.nextInt(width / 32);
-			int h = rng.nextInt(height / 32);
-			int x = rng.nextInt(width - w);
-			int y = rng.nextInt(height - h);
-			Rectangle r = new Rectangle(x, y, w, h);
-			r.setColor(100);
-			objects.add(r);
-			quadTree.insert(objects.get(i));
-		}
+    @Override
+    public void setup() {
+        t = millis();
+        surface.setTitle("QuadTreeDemo");
+        rng = new Random();
+        quadTree = new QuadTree(0, new Rectangle(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT), 5, 10);
+        objects = new ArrayList<>();
+        tempObjects = new ArrayList<>();
+        for (int i = 0; i < objectsNum; i++) {
+            int w = rng.nextInt(WINDOW_WIDTH / 32);
+            int h = rng.nextInt(WINDOW_HEIGHT / 32);
+            int x = rng.nextInt(WINDOW_WIDTH - w);
+            int y = rng.nextInt(WINDOW_HEIGHT - h);
+            Rectangle r = new Rectangle(x, y, w, h);
+            r.setColor(100);
+            objects.add(r);
+            quadTree.insert(objects.get(i));
+        }
 
-		noStroke();
+        noStroke();
 
-	}
+    }
 
-	public void input() {
-		if (mousePressed && mouseButton == 37) {
-			shouldColor = true;
-		} else {
-			shouldColor = false;
-		}
-		if (mousePressed && mouseButton == 39 && !drawingUserRect) {
-			userX = mouseX;
-			userY = mouseY;
-			drawingUserRect = true;
-		}
-		if (!mousePressed && drawingUserRect) {
-			userX2 = mouseX;
-			userY2 = mouseY;
-			drawingUserRect = false;
+    public void input() {
+        double userX2;
+        double userY2;
+        shouldColor = mousePressed && mouseButton == 37;
+        if (mousePressed && mouseButton == 39 && !drawingUserRect) {
+            userX = mouseX;
+            userY = mouseY;
+            drawingUserRect = true;
+        }
+        if (!mousePressed && drawingUserRect) {
+            userX2 = mouseX;
+            userY2 = mouseY;
+            drawingUserRect = false;
 
-			objectsNum++;
+            objectsNum++;
 
-			objects.add(new Rectangle(Math.min(userX, userX2), Math.min(userY,
-					userY2), Math.max(userX, userX2) - Math.min(userX, userX2),
-					Math.max(userY, userY2) - Math.min(userY, userY2)));
-		}
-		if(mousePressed&& mouseButton == 37&&drawingUserRect){
-			drawingUserRect=false;
-		}
-	}
+            objects.add(new Rectangle(Math.min(userX, userX2), Math.min(userY,
+                    userY2), Math.max(userX, userX2) - Math.min(userX, userX2),
+                    Math.max(userY, userY2) - Math.min(userY, userY2)));
+        }
+        if (mousePressed && mouseButton == 37 && drawingUserRect) {
+            drawingUserRect = false;
+        }
+    }
 
-	public void update(long dt) {
-		boolean update = false;
-		if (dt > 1000 / fps) {
-			update = true;
-			t = millis();
-		}
-		if (update) {
-		}
+    public void update(long dt) {
+        if (dt > 1000 / FPS) {
+            t = millis();
+        }
 
-		for (int i = 0; i < objectsNum; i++) {
-			objects.get(i).setColor(100);
-		}
-		frame.setTitle(actualfps + " QuadTreeDemo");
-		tempObjects.clear();
+        for (int i = 0; i < objectsNum; i++) {
+            objects.get(i).setColor(100);
+        }
+        surface.setTitle(actualFps + " QuadTreeDemo");
+        tempObjects.clear();
 
-		quadTree.clear();
-		for (Entity e : objects) {
-			quadTree.insert(e);
-		}
+        quadTree.clear();
+        for (Entity e : objects) {
+            quadTree.insert(e);
+        }
 
-		if (shouldColor) {
-			List<Entity> entities = quadTree.retrieve(tempObjects, mouseX,
-					mouseY);
-			for (Entity e : entities) {
-				e.setColor(250);
-			}
-			frame.setTitle(actualfps
-					+ " QuadTreeDemo: Need to check collision with only "
-					+ entities.size() + "/" + (objectsNum) + " objects.");
-		} else {
-			tempObjects.clear();
-			for (Entity e : objects) {
-				for (Entity e2 : quadTree.retrieve(tempObjects, e)) {
-					if (e != e2 && e.collidesWith(e2)) {
-						e.setColor(250);
-						e2.setColor(250);
-					}
-				}
-			}
-		}
+        if (shouldColor) {
+            List<Entity> entities = quadTree.retrieve(tempObjects, mouseX,
+                    mouseY);
+            for (Entity e : entities) {
+                e.setColor(250);
+            }
+            surface.setTitle(actualFps
+                    + " QuadTreeDemo: Need to check collision with only "
+                    + entities.size() + "/" + (objectsNum) + " objects.");
+        } else {
+            tempObjects.clear();
+            for (Entity e : objects) {
+                for (Entity e2 : quadTree.retrieve(tempObjects, e)) {
+                    if (e != e2 && e.collidesWith(e2)) {
+                        e.setColor(250);
+                        e2.setColor(250);
+                    }
+                }
+            }
+        }
 
-	}
+    }
 
-	public void draw() {
-		// update time stuff
-		long now = millis();
-		long dt = millis() - t;
-		t = now;
-		fpsDrawingDelay+=dt;
-		if (fpsDrawingDelay > 500) {
-			actualfps = (int) (1000 / dt);
-			fpsDrawingDelay=0;
-		}
+    @Override
+    public void draw() {
+        // update time stuff
+        long now = millis();
+        long dt = millis() - t;
+        t = now;
+        fpsDrawingDelay += dt;
+        if (fpsDrawingDelay > 500) {
+            actualFps = (int) (1000 / dt);
+            fpsDrawingDelay = 0;
+        }
 
-		// handle input
-		input();
+        // handle input
+        input();
 
-		// update logic
-		update(dt);
+        // update logic
+        update(dt);
 
-		// draw
-		background(50);
+        // draw
+        background(50);
 
-		fill(70,60,60, 125);
-		Entity e;
-		for (int i = 0; i < objectsNum; i++) {
-			stroke(objects.get(i).getColor());
-			e = objects.get(i);
-			rect((float) e.getX(), (float) e.getY(), (float) e.getWidth(),
-					(float) e.getHeight());
+        fill(70, 60, 60, 125);
+        Entity e;
+        for (int i = 0; i < objectsNum; i++) {
+            stroke(objects.get(i).getColor());
+            e = objects.get(i);
+            rect((float) e.getX(), (float) e.getY(), (float) e.getWidth(),
+                    (float) e.getHeight());
 
-		}
+        }
 
-		drawQuadTree(quadTree);
-		if (drawingUserRect) {
-			noStroke();
-			fill(160, 100, 150, 50);
-			rect((float) Math.min(userX, mouseX),
-					(float) Math.min(userY, mouseY),
-					(float) (Math.max(userX, mouseX) - Math.min(userX, mouseX)),
-					(float) (Math.max(userY, mouseY) - Math.min(userY, mouseY)));
-		}
-		noStroke();
+        drawQuadTree(quadTree);
+        if (drawingUserRect) {
+            noStroke();
+            fill(160, 100, 150, 50);
+            rect((float) Math.min(userX, mouseX),
+                    (float) Math.min(userY, mouseY),
+                    (float) (Math.max(userX, mouseX) - Math.min(userX, mouseX)),
+                    (float) (Math.max(userY, mouseY) - Math.min(userY, mouseY)));
+        }
+        noStroke();
 
-	}
+    }
 
-	private void drawQuadTree(QuadTree qt) {
-		stroke(100, 150, 100);
-		noFill();
-		rect((float) qt.getBounds().getX(), (float) qt.getBounds().getY(),
-				(float) qt.getBounds().getWidth(), (float) qt.getBounds()
-						.getHeight());
-		if (qt.hasChildren()) {
-			for (QuadTree qtc : qt.getChildren()) {
-				drawQuadTree(qtc);
-			}
-		}
+    private void drawQuadTree(QuadTree qt) {
+        stroke(100, 150, 100);
+        noFill();
+        rect((float) qt.getBounds().getX(), (float) qt.getBounds().getY(),
+                (float) qt.getBounds().getWidth(), (float) qt.getBounds()
+                        .getHeight());
+        if (qt.hasChildren()) {
+            for (QuadTree qtc : qt.getChildren()) {
+                drawQuadTree(qtc);
+            }
+        }
 
-	}
+    }
 
-	public void mousePressed() {
-		println(mouseButton);
-	}
+    @Override
+    public void mousePressed() {
+        println(mouseButton);
+    }
 
-	public void mouseReleased() {
-	}
+    @Override
+    public void keyPressed() {
+        println(keyCode);
 
-	public void keyPressed() {
-		println(keyCode);
+        // R
+        if (keyCode == 82) {
+            objects.clear();
+            objectsNum = 0;
+        }
 
-		// R
-		if (keyCode == 82) {
-			objects.clear();
-			objectsNum = 0;
-		}
+        // E
+        if (keyCode == 69) {
+            int scale = rng.nextInt(3) + 3;
+            int w = rng.nextInt((int) (WINDOW_WIDTH / (Math.pow(2, scale))));
+            int h = rng.nextInt((int) (WINDOW_HEIGHT / (Math.pow(2, scale))));
+            int x = rng.nextInt(WINDOW_WIDTH - w);
+            int y = rng.nextInt(WINDOW_HEIGHT - h);
+            Rectangle r = new Rectangle(x, y, w, h);
+            r.setColor(100);
+            objects.add(r);
+            objectsNum++;
+        }
+    }
 
-		// E
-		if (keyCode == 69) {
-			int scale = rng.nextInt(3) + 3;
-			int w = rng.nextInt((int) (width / (Math.pow(2, scale))));
-			int h = rng.nextInt((int) (height / (Math.pow(2, scale))));
-			int x = rng.nextInt(width - w);
-			int y = rng.nextInt(height - h);
-			Rectangle r = new Rectangle(x, y, w, h);
-			r.setColor(100);
-			objects.add(r);
-			objectsNum++;
-		}
-	}
 
-	public void keyReleased() {
-	}
+    public static void main(String[] passedArgs) {
+        String[] appletArgs = new String[]{"QuadTreeDemo.core.Core"};
+        if (passedArgs != null) {
+            PApplet.main(concat(appletArgs, passedArgs));
+        } else {
+            PApplet.main(appletArgs);
+        }
+    }
 
-	static public void main(String[] passedArgs) {
-		String[] appletArgs = new String[] { "QuadTreeDemo.core.Core" };
-		if (passedArgs != null) {
-			PApplet.main(concat(appletArgs, passedArgs));
-		} else {
-			PApplet.main(appletArgs);
-		}
-	}
+    @Override
+    public void settings() {
+        size(WINDOW_WIDTH, WINDOW_HEIGHT);
+    }
 
-	public void settings() {
-		size(width, height);
-	}
-
-	public static Core getInstance() {
-		return instance;
-	}
 }
--- QuadTreeDemo/entities/Ellipse.java
+++
@@ -1,9 +0,0 @@
-package QuadTreeDemo.entities;
-
-public class Ellipse extends Entity {
-
-	public Ellipse(double x, double y, double width, double height) {
-		super(x, y, width, height);
-	}
-
-}
